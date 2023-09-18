45c18654534dc89534a0522c488e1aaaaf27be80
Roman Bazaleev <bazaleev.roma@ya.ru>
2023-09-18 22:03:04 +0300

    window scale fixture remain

--- conftest.py
+++ conftest.py
@@ -10,4 +10,9 @@ def opening_browser_neg():
 def search_positive():
     browser.element('[class="ExCKkf z1asCe rzyADb"]').click()
     # browser.element('class="RNNXgb"').click()
-    browser.element('[name="Найти"]').should(be.blank).type('RedFab')
+    browser.element('[class="gLFyf"]').type('RedFab').press_enter()
+
+@pytest.fixture()
+def search_neagative_assert():
+    browser.element('[class="ExCKkf z1asCe rzyADb"]').click()
+    browser.element('[class="gLFyf"]').type('asdasdasfasfgsdagfdsgshfdghbfnjmkmjhdndgfjnfukl')
--- test_google.py
+++ test_google.py
@@ -9,3 +9,6 @@ def test_negative_search_result(opening_browser_neg):
 
 def test_positive_search_result(search_positive):
     assert browser.element('[id="search"]').should(have.text('Цифровое производство в Санкт-Петербурге'))
+
+def test_negative_assert_search_result(search_neagative_assert):
+    assert browser.element('[id="search"]').should(have.text('Цифровое производство в Санкт-Петербурге'))
